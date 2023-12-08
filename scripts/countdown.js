366cc5836dbbe20309cf8ba03027817064ba101b
Martin <gyurkes.martin@gmail.com>
2023-12-08 20:36:47 +0100

    Section about Us

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -96,11 +96,52 @@ function countfrom(in_time) {
 }
 
 function getName() {
-  if (invname == 'GyurkesZoltan') {
-    return "Papi!";
-  } else if (invname == 'LorinczFerenc') {
-    return "Feri!";
-  } else {
+  switch (invname) {
+  case 'Parents':
+    return 'Szüleink!';
+  case 'GrParents':
+    return 'Nagyszüleink!';
+  case 'VikiGabi':
+    return 'Viki és Gabi!';
+  case 'AndiSuni':
+    return 'Andi és Süni!';
+  case 'Vigyi':
+    return 'Vigyi!';
+  case 'TimkoRobi':
+    return 'Timkó, Robi és Levi!';
+  case 'KoriEdina':
+    return 'Edina, Köri, Máté és Bálint!';
+  case 'EsztiAndras':
+    return 'Eszti és András!';
+  case 'Adri':
+    return 'Adri!';
+  case 'Zsofi':
+    return 'Zsófi!';
+  case 'Anett':
+    return 'Anett!';
+  case 'PetraMatyi':
+    return 'Petra és Matyi!';
+  case 'ArpiAnna':
+    return 'Anna, Árpi és Lellebaba!';
+  case 'GeriBarbi':
+    return 'Barbi és Geri!';
+  case 'Misi':
+    return 'Misi!';
+  case 'Bence':
+    return 'Bence!';
+  case 'Adi':
+    return 'Virág és Ádi!'
+  case 'OliKata':
+    return 'Kata és Olivér!';
+  case 'LaliSzabcsi':
+    return 'Szabcsi és Lali!';
+  case 'Marko':
+    return 'Márkó!';
+  case 'Beni':
+    return 'Beni!';
+  case 'GaborFanni':
+    return 'Fanni és Gábor!';
+  default:
     return "Barátunk/Rokonunk!"
   }
 
